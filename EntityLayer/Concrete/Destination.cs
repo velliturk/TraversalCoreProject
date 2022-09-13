e809f2a0c67e4bbed0344b85591571073468a4b8
Veli Türk <veturk34@gmail.com>
2022-09-13 22:52:49 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Destination
+    {
+        public int DestinationID { get; set; }
+
+        public string City { get; set; }
+
+        // Kalınan Gece
+        public string DayNight { get; set; }
+
+        // Ücret
+        public double Price { get; set; }
+
+        // Container İçerisindeki Resim
+        public string Image { get; set; }
+
+        // Açıklama
+        public string Description { get; set; }
+
+        // Kapasite
+        public int Capacity { get; set; }
+
+        // Aktif mi Pasif mi
+        public bool Status { get; set; }
+    }
+}
